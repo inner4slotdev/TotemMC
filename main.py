ec44afa85036affd7733755282abe564dfcd5b1b
TotemMC <136920536+inner4slotdev@users.noreply.github.com>
2025-09-22 22:03:48 +0200

    Created main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/soon')
+def soon():
+    return render_template('soon.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
