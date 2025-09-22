8c6b2a3bbe98f8c1dae47ff18be4ef2c9682a42e
TotemMC <136920536+inner4slotdev@users.noreply.github.com>
2025-09-22 22:08:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/soon')
 def soon():
-    return render_template('soon.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
